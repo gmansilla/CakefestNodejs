8676c2bfff6096eaffa5f08404858ffd089a5d66
vagrant <vagrant@demo.cakefest>
2015-05-19 00:04:26 +0000

    deleting old code

--- app.js
+++ app.js
@@ -6,16 +6,6 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-
-app.get("/",function(req,res) {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-app.get("/notify", function(req, res) {
-    io.sockets.emit("something", {hello: 'emit'});
-});
-
 app.post('/articleNotification', function(req, res) {
 
 
