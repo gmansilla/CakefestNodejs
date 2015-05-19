f3911feaea1a87fce1fa19b998e174df4258f1cf
vagrant <vagrant@demo.cakefest>
2015-05-19 00:55:52 +0000

    format

--- app.js
+++ app.js
@@ -7,27 +7,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/articleNotification', function(req, res) {
-
-
     if (req.body.isNew === true) {
         io.sockets.emit('newArticle', req.body);
     }
-
     if (req.body.isNew === false) {
         io.sockets.emit('updatedArticle', req.body);
     }
-
     res.status(200).send('OK');
-
 });
-
-
 
 app.post('/newComment', function(req, res) {
     io.sockets.in(req.body.articleId).emit('newComment', req.body);
+    io.sockets.emit('notifyNewComment', req.body);
     res.status(200).send('OK');
 });
-
 
 io.on('connection', function(socket) {
     socket.on('joinRoom', function(data){
@@ -35,10 +28,6 @@ io.on('connection', function(socket) {
     });
 });
 
-
-
-
-
-http.listen(3000,function(){
+http.listen(3000, function() {
     console.log("Listening on 3000");
 });
